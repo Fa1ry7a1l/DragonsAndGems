05600ed464e5049f01a6e9e435e1267cbc367e63
andrey <Frosty.priense@gmail.com>
2022-12-06 23:03:08 +0300

    edited dungeon_master

--- db/migrate/20221206180610_create_rooms.rb
+++ db/migrate/20221206180610_create_rooms.rb
@@ -1,7 +1,7 @@
 class CreateRooms < ActiveRecord::Migration[7.0]
   def change
     create_table :rooms do |t|
-      t.references :dungeon_master_id, null: false, foreign_key: { to_table: :players }
+      t.references :dungeon_master, null: false, foreign_key: { to_table: :players }
 
       t.string :room_name, null: false
       t.references :character_1, foreign_key: { to_table: :characters_lists }
