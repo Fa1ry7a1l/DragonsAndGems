64c5e2f329cd0f8a1a90b54d7ab76a582316ad69
andrey <Frosty.priense@gmail.com>
2022-12-16 00:00:42 +0300

    redirect to login

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,6 @@
 class ApplicationController < ActionController::Base
-
+  def authenticate_user
+    @current_user = Player.find_by_id(session[:user_id])
+    redirect_to login_path unless @current_user.present?
+  end
 end
