ea707b19d62ee7032aa0d84e6d355b1055a324c2
Lyrai <x355x02c@live.com>
2022-12-18 17:41:29 +0300

    Added room join & find logic

--- app/controllers/room_controller.rb
+++ app/controllers/room_controller.rb
@@ -2,25 +2,47 @@ class RoomController < ApplicationController
   def new
   end
 
+  def generate_tag
+    room_tag = ""
+    (1..6).each { |i|
+      room_tag += rand(10).to_s
+    }
+
+    room_tag
+  end
+
   def find
   end
 
   def create
-    pp create_params
-    redirect_to "/room/#{create_params[:room_name]}"
+    master_id = session[:player_id]
+    room_name = create_params[:room_name]
+    room_tag = generate_tag
+    while Room.where(tag: room_tag).count != 0 do
+      room_tag = generate_tag
+    end
+
+    Room.create(dungeon_master_id: master_id, room_name: room_name, tag: room_tag)
+    redirect_to "/room/#{room_tag}"
   end
 
   def show
+    @lists = Player.where(id: session[:player_id])[0].characters_lists
   end
 
   def join
-    pp create_params
-    pp params
-    redirect_to "/room/#{create_params[:room_tag]}"
+    room_tag = create_params[:room_tag]
+    if Room.where(tag: room_tag).count != 0
+      redirect_to "/room/#{room_tag}"
+    else
+      flash[:error] = "Room not found"
+      redirect_to room_find_path
+    end
+
   end
 
   private
   def create_params
-    params.require(:room).permit(:room_name,:room_tag)
+    params.require(:room).permit(:room_name, :room_tag)
   end
 end
