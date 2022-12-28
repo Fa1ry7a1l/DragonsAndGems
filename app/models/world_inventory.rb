17f85eaad196578dc2319464e07c6eb02a833bc4
Allan <53708451+FrenchIceCream@users.noreply.github.com>
2022-12-28 11:13:34 +0300

    Fixed validations

--- app/models/world_inventory.rb
+++ app/models/world_inventory.rb
@@ -1,5 +1,3 @@
 class WorldInventory < ApplicationRecord
   validates :name, :description, presence: true
-  validates :weapon, presence: true, unless: :armor
-  validates :armor, presence: true, unless: :weapon
 end
